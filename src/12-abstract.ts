ba0d1fc1f805503139fc6355116f0a8dd1fea22b
marco-soria <marco.soriaa7@gmail.com>
2024-08-09 09:37:33 -0500

    abstract

---
+++ src/12-abstract.ts
@@ -0,0 +1,8 @@
+import { Animal, Cat } from './09-protected';
+
+//const animal = new Animal('elite');
+//animal.greeting();
+
+const tiger = new Cat('tiger', 'marco');
+tiger.greeting();
+tiger.meow(2);
